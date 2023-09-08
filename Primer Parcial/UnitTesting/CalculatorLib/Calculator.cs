7d43d69971eec5c9cdb10d62518fafdf6d18dc5f
MarioM <a20300683@ceti.mx>
2023-09-07 23:24:37 -0600

    Primer Parcial v4

---
+++ Primer Parcial/UnitTesting/CalculatorLib/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace CalculatorLib;
+
+public class Calculator
+{
+
+public double? Add(double?a, double?b)
+{
+
+        if(a+b > int.MaxValue ){
+            throw new OverflowException("Se excedio la memoria de int.");
+        }
+
+        else if(a == null || b == null)
+        {
+            throw new ArgumentNullException("Los parametros no pueden ser nulos");
+
+        }
+         else if(double.IsNaN(a.Value+b.Value) ||  double.IsInfinity(a.Value+b.Value)){
+            throw new NotFiniteNumberException("Resultado no es un número finito.");
+        }
+         else if(a+b ==23){
+            throw new ArithmeticException("Numero bloqueado.");
+        }
+        
+
+        return a+b;
+ 
+
+      
+}
+
+
+public double? Div(double ?a, double ?b)
+{
+  
+        if(b == 0){
+             throw new DivideByZeroException("No se puede dividir por cero.");
+        }
+        else if(a/b > double.MaxValue ){
+            throw new OverflowException("Se excedio la memoria de double.");
+        }
+          else if(a == null || b == null)
+        {
+            throw new ArgumentNullException("Los parametros no pueden ser nulos");
+
+
+        }
+        else if(double.IsNaN(a.Value/b.Value) ||  double.IsInfinity(a.Value/b.Value)){
+            throw new NotFiniteNumberException("Resultado no es un número finito.");
+        }
+       
+
+        return a/b;
+
+        
+    }
+   
+
+
+}
+
+
+
+    
+
---
+++ Primer Parcial/UnitTesting/CalculatorUnitTesting/CalculatorUnitTests.cs
@@ -0,0 +1,196 @@
+
+//UnitTset : 3 cosas necesarias
+// Arrange : Declara todo lo que necesito para correr mi prueba (objetos , variables , funciones)
+// Act : Es el que manda a llamar la funcion que quiero probar
+// Assert : Validar que lo que quiero de mi prueba , realmente se obtenga (comparar con lo esperado)
+
+
+namespace CalculatorUnitTests;
+
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using CalculatorLib;
+
+
+public class UnitTest1
+{
+
+    #region Division
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+
+    public void DivideByZeroExceptionDiv()
+    {
+
+        // Arrange
+        double? a = 23;
+        double? b = 0.0;
+
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<DivideByZeroException>(() => calc.Div(a, b));
+
+
+    }
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void ArgumentNullExceptionDiv()
+    {
+        // Arrange
+        double? a = 7;
+        double? b = null;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<ArgumentNullException>(() => calc.Div(a, b));
+
+
+
+    }
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void NotFiniteNumberExceptionDiv()
+    {
+        // Arrange
+        double? a = double.NaN;
+        double? b = double.PositiveInfinity;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<NotFiniteNumberException>(() => calc.Div(a, b));
+
+
+    }
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void OverflowExceptionDiv()
+    {
+        // Arrange
+        double? a = double.MaxValue;
+        double? b = 0.1;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<OverflowException>(() => calc.Div(a, b));
+
+
+
+    }
+
+    #endregion
+
+
+    #region Suma
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void ArgumentNullExceptionAdd()
+    {
+        // Arrange
+        double? a = 1.0;
+        double? b = null;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<ArgumentNullException>(() => calc.Add(a, b));
+
+
+
+    }
+
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void OverflowExceptionAdd()
+    {
+        // Arrange
+        double? a = double.MaxValue;
+        double? b = 1.0;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<OverflowException>(() => calc.Add(a, b));
+
+
+    }
+
+    [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    public void NotFiniteNumberExceptionAdd()
+    {
+        // Arrange
+        double? a = double.NegativeInfinity;
+        double? b = -1.0;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<NotFiniteNumberException>(() => calc.Add(a, b));
+
+
+    }
+      [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+    
+      public void AritmethicExceptionAdd()
+    {
+        // Arrange
+        double? a = 11.0;
+        double? b = 12.0;
+        Calculator calc = new();
+
+        // Act y Assert
+        Assert.Throws<ArithmeticException>(() => calc.Add(a, b));
+
+
+
+    }
+
+
+
+
+    #endregion
+
+
+
+
+    /*
+        #region verif
+
+
+
+         [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+         public  void VerifAdd(){
+            // Arrange
+            string a = "10";
+            string b = "1";
+            double expect = double.Parse(a) + double.Parse(b);
+            Calculator calc = new();
+
+            Assert.Equal(expect,calc.Add(a,b));
+
+
+
+        }
+
+        [Fact] //Annotation : Verbos que le puedo poner a mis funcione
+         public  void VerifDiv(){
+            // Arrange
+            string a = "20.5";
+            string b = "10";
+            double expect = double.Parse(a) / double.Parse(b);
+            Calculator calc = new();
+
+
+            Assert.Equal(expect,calc.Div(a,b));
+
+
+
+
+
+        }
+
+
+
+        #endregion
+
+        */
+
+
+
+}
